d12e27d5455b0897e6229d5a9e06f0574b3cc14a
Reuben <gathiireuben@gmail.com>
2024-06-15 08:37:29 +0300

    Created sample tasks

---
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -0,0 +1,9 @@
+package com.example.model
+
+//Represents sample values for the tasks
+val tasks = mutableListOf(
+    Task("cleaning", "Clean the house", Priority.Low),
+    Task("gardening", "Mow the lawn", Priority.Medium),
+    Task("shopping", "Buy the groceries", Priority.High),
+    Task("painting", "Paint the fence", Priority.Medium)
+)
