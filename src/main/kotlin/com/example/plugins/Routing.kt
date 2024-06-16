1514cead5b3043a54dcd9948f3b9520b46c8d693
Reuben <gathiireuben@gmail.com>
2024-06-16 12:05:19 +0300

    Added a route to view tasks by priority

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.model.Priority
 import com.example.model.TaskRepository
 import com.example.model.taskAsTable
 import io.ktor.http.*
@@ -9,12 +10,38 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get {
+
+        get("/tasks") {
             val tasks = TaskRepository.allTasks()
             call.respondText(
                 contentType = ContentType.parse("text/html"),
                 text = tasks.taskAsTable()
             )
+        }
+
+        get("/tasks/byPriority/{priority}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null){
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.taskByPriority(priority)
+
+                if (tasks.isEmpty()){
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.taskAsTable()
+                )
+            } catch (ex: IllegalArgumentException){
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
